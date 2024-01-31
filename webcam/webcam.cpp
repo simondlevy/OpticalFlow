fcc0bc2a71f87ffcc0ec0ed76c2a0c516c4ff46a
simondlevy <simon.d.levy@gmail.com>
2024-01-30 19:20:34 -0500

    modified:   webcam.cpp

--- webcam/webcam.cpp
+++ webcam/webcam.cpp
@@ -14,12 +14,44 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include<opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+#include <stdio.h>
 
-#include <opticalflow.hpp>
+using namespace cv;
+using namespace std;
 
-int main(int argc, char ** argv)
+int main(int, char**)
 {
+    Mat frame;
+    
+    VideoCapture cap;
+
+    int deviceID = 0;             
+    int apiID = cv::CAP_ANY;      
+    
+    cap.open(deviceID, apiID);
+
+    if (!cap.isOpened()) {
+        cerr << "ERROR! Unable to open camera\n";
+        return -1;
+    }
+    
+    while (true)
+    {
+        cap.read(frame);
+        
+        if (frame.empty()) {
+            cerr << "ERROR! blank frame grabbed\n";
+            break;
+        }
+        
+        imshow("Live", frame);
+        if (waitKey(5) >= 0)
+            break;
+    }
+    
     return 0;
 }
-
