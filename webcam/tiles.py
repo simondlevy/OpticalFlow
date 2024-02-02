7559c95a2423d39b1d009370d082cfdcf7e1dd62
simondlevy <simon.d.levy@gmail.com>
2024-02-01 19:30:36 -0500

    =More tiling work

--- webcam/tiles.py
+++ webcam/tiles.py
@@ -4,7 +4,8 @@ R = 14
 C = 12
 P = 2
 
-for k in range(R * C):
-    r = k // (P * C)
-    c = (k % C) // P
-    print('%3d: %d %d' % (k, r, c))
+for j in range(R * C):
+    r = j // (P * C)
+    c = (j % C) // P
+    k = j % (P*P) 
+    print('%3d: %d %d %d' % (j, r, c, k))
